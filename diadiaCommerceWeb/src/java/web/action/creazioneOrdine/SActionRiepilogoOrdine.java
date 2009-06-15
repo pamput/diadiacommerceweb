71c69edb7dce238d76a4a64c5c1203384f270c67
Kamal.KamalMohamed <Kamal.KamalMohamed@3494e902-56a1-11de-8d7d-01d876f1962d>
2009-06-15 15:49:13 +0000

    1- Implementazione del caso d'uso UC3 (consulta ordini) nella pagina perrsonale dell'utente (Non Terminato) 2- Modifica delle tabelle presenti nelle pagine web dell'applicazione, ora il parametro border viene settato tramite css

--- diadiaCommerceWeb/src/java/web/action/creazioneOrdine/SActionRiepilogoOrdine.java
+++ diadiaCommerceWeb/src/java/web/action/creazioneOrdine/SActionRiepilogoOrdine.java
@@ -5,6 +5,7 @@
 
 package web.action.creazioneOrdine;
 
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -17,10 +18,10 @@ import org.apache.struts.action.ActionForward;
  *
  * @author Kimo
  */
-public class SActionAggiungiProdotto extends org.apache.struts.action.Action {
+public class SActionRiepilogoOrdine extends org.apache.struts.action.Action {
     
     /* forward name="success" path="" */
-    private final static String SUCCESS = "success";
+    private final static String SUCCESS = "mostraRiepilogoOrdine";
     
     /**
      * This is the action called from the Struts framework.
@@ -31,10 +32,9 @@ public class SActionAggiungiProdotto extends org.apache.struts.action.Action {
      * @throws java.lang.Exception
      * @return
      */
-    public ActionForward execute(ActionMapping mapping, ActionForm form,
-            HttpServletRequest request, HttpServletResponse response)
-            throws Exception {
-        
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        //TODO deve prendere dalla sessione il catalogoProdotti e le property quantitaX per generare l'oggetto ordine (e infilarlo poi in sessione)
         return mapping.findForward(SUCCESS);
     }
 }
